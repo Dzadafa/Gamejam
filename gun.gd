0c447885eb2b7b97cab0d7eaa78dfaecb97adb55
fathursn21 <faturrosin333@gmail.com>
2026-04-05 20:32:46 +0700

    feat: prototype-player-shooter

---
+++ gun.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var bullet_scene : PackedScene
+@onready var bullet_spawn = $BulletSpawn
+@onready var bullet_target = $BulletTarget
+@onready var timer = $ReloadTimer
+var isShooting : bool = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.wait_time = 0.25
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("klik_kiri_mouse"):
+		print("tembak")
+		if isShooting:
+			shoot()
+	if Input.is_action_just_pressed("klik_kiri_mouse"):
+		shoot()
+	pass
+	
+func shoot():   
+	var bullet = bullet_scene.instantiate()
+	var shoot_direction = (bullet_target.global_position - bullet_spawn.global_position).normalized()
+	
+	isShooting = false
+	get_tree().current_scene.add_child(bullet)
+	
+	bullet.global_position = bullet_spawn.global_position
+	bullet.bullet_direction = shoot_direction
+	bullet.look_at(global_position)
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	isShooting = true
+	pass # Replace with function body.
--- player_movement.gd
+++ player_movement.gd
@@ -4,20 +4,15 @@ extends CharacterBody2D
 const SPEED = 400.0
 const ACCELERATION = SPEED * 5
 const FRICTION = SPEED * 4
-#const JUMP_VELOCITY = -400.0
+
+@export var bullet_scene : PackedScene
+@onready var gun_marker = $GunPivot
+@onready var gun = $Gun
+
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
@@ -25,3 +20,12 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide()
+	gun_rotation()
+		
+func gun_rotation():
+	var mouse_position = get_global_mouse_position()
+	gun.look_at(mouse_position)
+	if mouse_position.x < global_position.x:
+		gun.scale.y = -1 # Balik sumbu Y agar tidak upside down
+	else:
+		gun.scale.y = 1
