b651faac4206486ac0346158da931abe4689b1c1
Dzadaafa <dzadafa77@gmail.com>
2026-04-11 16:01:59 +0700

    finished the scene, need some music

--- scripts/cutscenes/ending.gd
+++ scripts/cutscenes/ending.gd
@@ -1,18 +1,26 @@
 extends Node2D
 
+var animation_player 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("dna_scan")
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.play("RESET")
-	$AnimationPlayer.play("reveal")
-	await $AnimationPlayer.animation_finished
-	$AnimationPlayer.play("RESET")
-	$AnimationPlayer.play("lose")
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func normal_ending():
+	animation_player = $AnimationPlayer
+	$AnimationPlayer.play("dna_scan")
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("reveal")
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("lose")
+	await $AnimationPlayer.animation_finished
+	
+func loser_ending():
+	
+	
+	
