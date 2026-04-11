05fd89cf5e9cf104b4980ddc62c4961107b9ffb2
Dzadaafa <dzadafa77@gmail.com>
2026-04-11 21:40:51 +0700

    finishing: ending

--- scripts/cutscenes/ending.gd
+++ scripts/cutscenes/ending.gd
@@ -1,12 +1,11 @@
 extends Node2D
+class_name ending
 
 var animation_player 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await normal_ending()
-	await loser_ending()
-	await easter_ending()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,11 +21,14 @@ func normal_ending():
 	await $AnimationPlayer.animation_finished
 	
 func loser_ending():
-	animation_player = $AnimationPlayer
 	$AnimationPlayer.play("loser")
 	await $AnimationPlayer.animation_finished
 	
-func easter_ending():
+func easter_egg():
 	animation_player = $AnimationPlayer
+	$AnimationPlayer.play("dna_scan")
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("reveal")
+	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("easter_egg")
 	await $AnimationPlayer.animation_finished
