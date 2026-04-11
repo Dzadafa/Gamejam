2390466568b1663c84902a37bbd6a33f0a9aef75
ShaffaDwiaji <amagiri7141@google.com>
2026-04-11 19:05:45 +0700

    add: sounds for some ending

--- scripts/cutscenes/ending.gd
+++ scripts/cutscenes/ending.gd
@@ -4,8 +4,9 @@ var animation_player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	await normal_ending()
+	await loser_ending()
+	await easter_ending()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,6 +22,11 @@ func normal_ending():
 	await $AnimationPlayer.animation_finished
 	
 func loser_ending():
+	animation_player = $AnimationPlayer
+	$AnimationPlayer.play("loser")
+	await $AnimationPlayer.animation_finished
 	
-	
-	
+func easter_ending():
+	animation_player = $AnimationPlayer
+	$AnimationPlayer.play("easter_egg")
+	await $AnimationPlayer.animation_finished
